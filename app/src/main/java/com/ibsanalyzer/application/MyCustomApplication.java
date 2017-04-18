9c9a7859e73d541d34ff51c8e20400d6a49fe5cd
Johan Lund <jolu1207@student.miun.se>
2017-04-18 09:28:07 +0200

    Implementing tablayout and fragments

--- app/src/main/java/com/ibsanalyzer/application/MyCustomApplication.java
+++ app/src/main/java/com/ibsanalyzer/application/MyCustomApplication.java
@@ -1,6 +1,8 @@
 package com.ibsanalyzer.application;
 
 import android.app.Application;
+import android.os.StrictMode;
+import android.util.Config;
 
 import com.jakewharton.threetenabp.AndroidThreeTen;
 
@@ -9,9 +11,28 @@ import com.jakewharton.threetenabp.AndroidThreeTen;
  */
 
 public class MyCustomApplication extends Application {
+    private static final boolean DEVELOPER_MODE = false;
     @Override
     public void onCreate() {
+        if (DEVELOPER_MODE) {
+            StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder()
+                    .detectDiskReads()
+                    .detectDiskWrites()
+                    .detectNetwork()   // or .detectAll() for all detectable problems
+                    .penaltyLog()
+                    .build());
+            StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder()
+                    .detectLeakedSqlLiteObjects()
+                    .detectLeakedClosableObjects()
+                    .penaltyLog()
+                    .penaltyDeath()
+                    .build());
+        }
+
         super.onCreate();
+
+
+
         AndroidThreeTen.init(this);
     }
 }
---
+++ app/src/main/java/com/ibsanalyzer/inputday/TabPagerAdapter.java
@@ -0,0 +1,40 @@
+package com.ibsanalyzer.inputday;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by Johan on 2017-04-18.
+ * From Android Studio Development Essentials p. 334
+ */
+
+public class TabPagerAdapter extends FragmentPagerAdapter {
+    int tabCount;
+
+    public TabPagerAdapter(FragmentManager fm, int nrOfTabs){
+        super(fm);
+        this.tabCount = nrOfTabs;
+    }
+    @Override
+    public Fragment getItem(int position) {
+        switch(position){
+            case 0:
+                TemplateFragment tabT = new TemplateFragment();
+                return tabT;
+            case 1:
+                DiaryFragment tabD = new DiaryFragment();
+                return tabD;
+            case 2:
+                StatFragment tabS = new StatFragment();
+                return tabS;
+            default:
+                return null;
+        }
+    }
+
+    @Override
+    public int getCount() {
+        return tabCount;
+    }
+}
