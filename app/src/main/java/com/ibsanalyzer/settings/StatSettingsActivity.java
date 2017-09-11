b8330960735f2abf94f6700870350f7143647528
Johan Lund <jolu1207@student.miun.se>
2017-09-11 12:46:05 +0200

    removed done button from preference activity since backbutton also accepts the values

--- app/src/main/java/com/ibsanalyzer/settings/StatSettingsActivity.java
+++ app/src/main/java/com/ibsanalyzer/settings/StatSettingsActivity.java
@@ -1,51 +1,28 @@
 package com.ibsanalyzer.settings;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
-import android.view.Menu;
-import android.view.MenuInflater;
 import android.view.MenuItem;
 
 import com.ibsanalyzer.inputday.R;
 
 
 public class StatSettingsActivity extends AppCompatPreferenceActivity {
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.done_menu, menu);
-        menu.findItem(R.id.menu_done).setOnMenuItemClickListener(new MenuItem
-                .OnMenuItemClickListener() {
-
-            @Override
-            public boolean onMenuItemClick(MenuItem item) {
-                doneClicked();
-                return true;
-            }
-        });
-        return true;
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getFragmentManager().beginTransaction()
-                .replace(android.R.id.content, new MyPreferenceFragment())
+                .replace(android.R.id.content, new StatPreferenceFragment())
                 .commit();
     }
 
-    public static class MyPreferenceFragment extends PreferenceFragment {
+    public static class StatPreferenceFragment extends PreferenceFragment {
         @Override
         public void onCreate(final Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             addPreferencesFromResource(R.xml.preferences);
         }
-    }
-
-    private void doneClicked() {
-        //store variables to database here
-        super.finish();
     }
 
     public boolean onOptionsItemSelected(MenuItem item) {
