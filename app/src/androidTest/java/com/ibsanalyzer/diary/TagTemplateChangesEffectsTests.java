076e25b4cc50a4228329b87f031b26d920090c34
Johan Lund <jolu1207@student.miun.se>
2017-12-31 18:32:49 +0100

    testet fungerar, kraschar där det ska

--- app/src/androidTest/java/com/ibsanalyzer/diary/TagTemplateChangesEffectsTests.java
+++ app/src/androidTest/java/com/ibsanalyzer/diary/TagTemplateChangesEffectsTests.java
@@ -1,14 +1,19 @@
 package com.ibsanalyzer.diary;
 
 import android.support.test.espresso.ViewInteraction;
+import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.ibsanalyzer.base_classes.Event;
 import com.ibsanalyzer.drawer.DrawerActivity;
+import com.ibsanalyzer.help_classes.RecyclerViewMatcher;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.Map;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
@@ -16,14 +21,23 @@ import static android.support.test.espresso.Espresso.pressBack;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.action.ViewActions.scrollTo;
 import static android.support.test.espresso.assertion.ViewAssertions.doesNotExist;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static com.ibsanalyzer.diary.R.id.events_layout;
 import static com.ibsanalyzer.diary.R.id.textView;
+import static com.ibsanalyzer.drawer.TestFromEspressoRecordning.childAtPosition;
 import static org.hamcrest.Matchers.allOf;
 import static org.hamcrest.Matchers.anything;
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.instanceOf;
+import static org.hamcrest.Matchers.is;
 
 /**
  * Created by Johan on 2017-12-31.
@@ -36,7 +50,7 @@ public class TagTemplateChangesEffectsTests {
                 .class);
 
         @Test
-        public void deletedTagTemplateBackBtnTest() {
+        public void deletedTagTemplateBackBtnTest() throws InterruptedException {
             //create a new TagTemplate and add it to an OtherEvent and click done in TagAdderActivity and in OtherActivity so it is placed in diary.
             onView(allOf(withId(R.id.otherBtn), isDisplayed())).perform(click());
             onView(withId(R.id.addTagsBtn)).perform(click());
@@ -47,13 +61,11 @@ public class TagTemplateChangesEffectsTests {
             onView(withId(R.id.menu_done)).perform(click());
 
             //Inside DiaryFragment, check that the tag has been added.
-            onView(allOf(withId(R.id.stattagname), withText("Butter"))).check(matches(isDisplayed()));
-
+            onView(allOf(withId(R.id.events_layout), isDisplayed())).check(matches(hasDescendant(withText(containsString("Butter")))));
+            
             //go back into the event
-            onData(anything())
-                    .inAdapterView(withId(R.id.events_layout))
-                    .atPosition(0)
-                    .perform(click());
+            onView(allOf(isDisplayed(), withId(R.id.events_layout)))
+                    .perform(RecyclerViewActions.actionOnItemAtPosition(0, click()));
 
             //go into add tags and delete the TagTemplate.
             onView(withId(R.id.addTagsBtn)).perform(click());
@@ -93,5 +105,8 @@ public class TagTemplateChangesEffectsTests {
         //Press done.
 
         //You should now be inside DiaryFragment (check that it is the same date as before), and only the new tag should be found, not the deleted one..
+    }
+    public static RecyclerViewMatcher withRecyclerView(final int recyclerViewId) {
+        return new RecyclerViewMatcher(recyclerViewId);
     }
 }
