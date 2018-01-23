dcff32fe9ca3c28bd9a2a250d218a397675b7571
Johan Lund <jolu1207@student.miun.se>
2018-01-23 18:15:36 +0100

    design ready for portions settings

--- app/src/main/java/com/ibsanalyzer/statistics/NewPortionRangeActivity.java
+++ app/src/main/java/com/ibsanalyzer/statistics/NewPortionRangeActivity.java
@@ -2,8 +2,11 @@ package com.ibsanalyzer.statistics;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
-import com.ibsanalyzer.inputday.R;
+import com.ibsanalyzer.diary.R;
 
 public class NewPortionRangeActivity extends AppCompatActivity {
 
@@ -11,5 +14,24 @@ public class NewPortionRangeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_portion_range);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.done_menu, menu);
+        menu.findItem(R.id.menu_done).setOnMenuItemClickListener(new MenuItem
+                .OnMenuItemClickListener() {
+
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                doneClicked();
+                return true;
+            }
+        });
+        return true;
+    }
+
+    private void doneClicked(){
+
     }
 }
