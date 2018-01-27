01de9c108b34734270ea109c4dd1f6cb9cf09fff
Johan Lund <jolu1207@student.miun.se>
2018-01-27 14:15:15 +0100

    added numberpicker

--- app/src/main/java/com/ibsanalyzer/statistics/NewPortionRangeActivity.java
+++ app/src/main/java/com/ibsanalyzer/statistics/NewPortionRangeActivity.java
@@ -1,19 +1,40 @@
 package com.ibsanalyzer.statistics;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.ibsanalyzer.diary.R;
+import com.ibsanalyzer.util.Util;
 
 public class NewPortionRangeActivity extends AppCompatActivity {
-
+    TextView from;
+    TextView to;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_portion_range);
+        from = (TextView)findViewById(R.id.fromRangePortions);
+        final Activity here = this;
+        from.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Util.useNumberPickerDialog(here, from);
+            }
+        });
+
+        to = (TextView)findViewById(R.id.toRangePortions);
+        to.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Util.useNumberPickerDialog(here, to);
+            }
+        });
     }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
