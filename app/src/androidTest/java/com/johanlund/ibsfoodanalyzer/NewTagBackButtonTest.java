63d7af779a14c93452707adee1a004c5b3e6ae88
Johan Lund <jolu1207@student.miun.se>
2018-04-03 08:43:56 +0200

    updated tests

--- app/src/androidTest/java/com/johanlund/ibsfoodanalyzer/NewTagBackButtonTest.java
+++ app/src/androidTest/java/com/johanlund/ibsfoodanalyzer/NewTagBackButtonTest.java
@@ -1,14 +1,23 @@
 package com.johanlund.ibsfoodanalyzer;
 
 
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.johanlund.base_classes.Tag;
+import com.johanlund.database.DBHandler;
+import com.johanlund.help_classes.AndroidTestUtil;
 import com.johanlund.main.DrawerActivity;
+import com.johanlund.model.TagType;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.Espresso.pressBack;
@@ -25,8 +34,30 @@ public class NewTagBackButtonTest {
     public ActivityTestRule<DrawerActivity> mActivityTestRule = new ActivityTestRule<>(DrawerActivity
             .class);
 
+    @Before
+    public void clearDatabase(){
+        AndroidTestUtil.clearDatabaseByClicking();
+    }
+
     @Test
-    public void newTagBackButtonTest() {
+    public void newTagBackButtonWhenThereAreNoTagsAddedBeforeTest() {
+        onView(allOf(withId(R.id.mealBtn), isDisplayed())).perform(click());
+        onView(withId(R.id.addTagsBtn)).perform(click());
+
+        //inside TagAdderActivity, there should now be an information text how to add tags.
+        //pressing back button to go back to MealActivity
+        pressBack();
+
+        //should be back in MealActivity, check for some id there
+        onView(withId(R.id.mealContainer)).check(matches(isDisplayed()));
+    }
+    @Test
+    public void newTagBackButtonWhenThereAreTagsAddedTest() {
+        //add a tag type...
+        DBHandler dbHandler = new DBHandler(InstrumentationRegistry.getTargetContext());
+        TagType tt = new TagType("yoghurt");
+        dbHandler.addTagTemplate(tt);
+
         onView(allOf(withId(R.id.mealBtn), isDisplayed())).perform(click());
         onView(withId(R.id.addTagsBtn)).perform(click());
 
