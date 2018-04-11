30efa3b538e30237b611f72ab414a6f191a95f60
Johan Lund <jolu1207@student.miun.se>
2018-04-11 15:07:25 +0200

    added failing test

--- app/src/test/java/com/johanlund/base_classes/ChunkTests.java
+++ app/src/test/java/com/johanlund/base_classes/ChunkTests.java
@@ -55,7 +55,6 @@ public class ChunkTests {
         Break bSameTime = new Break(newYear);
         Rating rAfterB = new Rating(newYear.plusHours(1), 3);
 
-
         List<Event>events = new ArrayList<>();
         events.add(rStart);
         events.add(rAfterB);
@@ -65,5 +64,50 @@ public class ChunkTests {
         assertEquals(2, chunks.size());
         assertEquals(1, chunks.get(0).getEvents().size());
         assertEquals(1, chunks.get(1).getEvents().size());
+    }
+    @Test
+    public void canMakeThreeChunks() {
+        Rating rStart = new Rating(newYear, 3);
+        Break bAfterStart = new Break(newYear.plusHours(1));
+        Rating rMiddle = new Rating(newYear.plusHours(2), 3);
+        Break bBeforeEnd = new Break(newYear.plusHours(3));
+        Rating rEnd = new Rating(newYear.plusHours(4), 3);
+
+        List<Event>events = new ArrayList<>();
+        events.add(rStart);
+        events.add(rMiddle);
+        events.add(rEnd);
+
+        List<Break>breaks = Arrays.asList(bAfterStart, bBeforeEnd);
+        List<Chunk>chunks = Chunk.makeChunksFromEvents(events, breaks);
+
+        assertEquals(3, chunks.size());
+        assertEquals(1, chunks.get(0).getEvents().size());
+        assertEquals(1, chunks.get(1).getEvents().size());
+        assertEquals(1, chunks.get(2).getEvents().size());
+    }
+    //this case can actually happen if 2 events have same time, and they both have breaks set on them.
+    @Test
+    public void twoBreaksBetweenEventsShouldActLikeTheyWereOne() {
+        Rating rStart1 = new Rating(newYear, 3);
+        Rating rStart2 = new Rating(newYear, 3);
+        Break bAfterStart1 = new Break(newYear);
+        Break bAfterStart2 = new Break(newYear);
+        Rating rMiddle = new Rating(newYear.plusHours(2), 3);
+        Break bBeforeEnd = new Break(newYear.plusHours(3));
+        Rating rEnd = new Rating(newYear.plusHours(4), 3);
+
+        List<Event>events = new ArrayList<>();
+        events.add(rStart1);
+        events.add(rStart2);
+        events.add(rMiddle);
+        events.add(rEnd);
+        List<Break>breaks = Arrays.asList(bAfterStart1, bAfterStart2, bBeforeEnd);
+        List<Chunk>chunks = Chunk.makeChunksFromEvents(events, breaks);
+
+        assertEquals(3, chunks.size());
+        assertEquals(2, chunks.get(0).getEvents().size());
+        assertEquals(1, chunks.get(1).getEvents().size());
+        assertEquals(1, chunks.get(2).getEvents().size());
     }
 }
