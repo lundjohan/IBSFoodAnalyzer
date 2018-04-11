6386cf7396bf8c4cbb85693d35e6ef733e09ca34
Johan Lund <jolu1207@student.miun.se>
2018-04-11 12:07:46 +0200

    added failing test

--- app/src/test/java/com/johanlund/base_classes/ChunkTests.java
+++ app/src/test/java/com/johanlund/base_classes/ChunkTests.java
@@ -13,10 +13,24 @@ import static junit.framework.Assert.assertEquals;
 public class ChunkTests {
     private static LocalDateTime newYear = LocalDateTime.of(2018, Month.JANUARY, 1, 0, 0);
     @Test
-    public void testThatNoChunkIsCreatedIfNoEvents() {
+    public void testThatNoChunkIsCreatedIfNoEventsButOneBreak() {
         List<Event>events = new ArrayList<>();
         List<Break>breaks = Arrays.asList(new Break(newYear));
         List<Chunk>chunks = Chunk.makeChunksFromEvents(events, breaks);
         assertEquals(0, chunks.size());
+    }
+    @Test
+    public void testEventsBecomesTwoChunks() {
+        Rating beforeSplit = new Rating(newYear, 3);
+        Break splitter = new Break(newYear.plusHours(1));
+        Rating afterSplit = new Rating(newYear.plusHours(2), 3);
+
+        List<Event>events = new ArrayList<>();
+        events.add(beforeSplit);
+        events.add(afterSplit);
+        List<Break>breaks = Arrays.asList(splitter);
+        List<Chunk>chunks = Chunk.makeChunksFromEvents(events, breaks);
+
+        assertEquals(2, chunks.size());
     }
 }
