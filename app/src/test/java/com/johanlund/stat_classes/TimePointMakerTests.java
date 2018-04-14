3065023f845d33524ca8d1738596947a4a426aa1
Johan Lund <jolu1207@student.miun.se>
2018-04-14 13:41:23 +0200

    adding failed test

--- app/src/test/java/com/johanlund/stat_classes/TimePointMakerTests.java
+++ app/src/test/java/com/johanlund/stat_classes/TimePointMakerTests.java
@@ -34,9 +34,20 @@ public class TimePointMakerTests {
         assertEquals(2, tps.size());
         assertEquals(newYear, tps.get(0).getStartTime());
         assertEquals(newYear.plusHours(3), tps.get(0).getStopTime());
-        assertEquals(3, tps.get(0).getDurationInHours()); //check getDuration is ok
+        assertEquals(3, tps.get(0).getDurationInHours()); //check getDuration is working
 
         assertEquals(newYear.plusHours(10), tps.get(1).getStartTime());
         assertEquals(newYear.plusHours(20), tps.get(1).getStopTime());
+    }
+    @Test
+    public void shouldReturnMiddlePartOnly(){
+        Rating r1 = new Rating(newYear, 6);
+        Rating r2 = new Rating(newYear.plusHours(3), 3);
+        Rating r3 = new Rating(newYear.plusHours(10), 6);
+
+        List<TimePoint> tps =  TimePointMaker.doTimePoint(Arrays.asList(r1, r2, r3), newYear.plusHours(20), 1, 4);
+        assertEquals(1, tps.size());
+        assertEquals(newYear.plusHours(3), tps.get(0).getStartTime());
+        assertEquals(newYear.plusHours(10), tps.get(0).getStopTime());
     }
 }
