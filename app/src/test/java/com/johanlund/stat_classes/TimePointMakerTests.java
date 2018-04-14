84b614fdc65182b7df341abead07eef53782dbaa
Johan Lund <jolu1207@student.miun.se>
2018-04-14 13:53:36 +0200

    all tests pass

--- app/src/test/java/com/johanlund/stat_classes/TimePointMakerTests.java
+++ app/src/test/java/com/johanlund/stat_classes/TimePointMakerTests.java
@@ -50,4 +50,40 @@ public class TimePointMakerTests {
         assertEquals(newYear.plusHours(3), tps.get(0).getStartTime());
         assertEquals(newYear.plusHours(10), tps.get(0).getStopTime());
     }
+    @Test
+    public void shouldReturnFirstAndSecondPartAsOne(){
+        Rating r1 = new Rating(newYear, 6);
+        Rating r2 = new Rating(newYear.plusHours(3), 5);
+        Rating r3 = new Rating(newYear.plusHours(10), 3);
+
+        List<TimePoint> tps =  TimePointMaker.doTimePoint(Arrays.asList(r1, r2, r3), newYear.plusHours(20), 5, 6);
+
+        assertEquals(1, tps.size());
+        assertEquals(newYear, tps.get(0).getStartTime());
+        assertEquals(newYear.plusHours(10), tps.get(0).getStopTime());
+    }
+    @Test
+    public void shouldReturnSecondAndThirdPartAsOne(){
+        Rating r1 = new Rating(newYear, 6);
+        Rating r2 = new Rating(newYear.plusHours(3), 5);
+        Rating r3 = new Rating(newYear.plusHours(10), 3);
+
+        List<TimePoint> tps =  TimePointMaker.doTimePoint(Arrays.asList(r1, r2, r3), newYear.plusHours(20), 3, 5);
+
+        assertEquals(1, tps.size());
+        assertEquals(newYear.plusHours(3), tps.get(0).getStartTime());
+        assertEquals(newYear.plusHours(20), tps.get(0).getStopTime());
+    }
+    @Test
+    public void shouldReturnEverythingAsOne(){
+        Rating r1 = new Rating(newYear, 6);
+        Rating r2 = new Rating(newYear.plusHours(3), 5);
+        Rating r3 = new Rating(newYear.plusHours(10), 3);
+
+        List<TimePoint> tps =  TimePointMaker.doTimePoint(Arrays.asList(r1, r2, r3), newYear.plusHours(20), 3, 6);
+
+        assertEquals(1, tps.size());
+        assertEquals(newYear, tps.get(0).getStartTime());
+        assertEquals(newYear.plusHours(20), tps.get(0).getStopTime());
+    }
 }
