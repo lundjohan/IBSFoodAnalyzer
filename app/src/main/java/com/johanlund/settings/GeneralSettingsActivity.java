0e34afdad7e71a0f3b0850813b70d102bc071938
Johan Lund <jolu1207@student.miun.se>
2018-04-27 11:58:19 +0200

    settings pref to default values works, but not visually

--- app/src/main/java/com/johanlund/settings/GeneralSettingsActivity.java
+++ app/src/main/java/com/johanlund/settings/GeneralSettingsActivity.java
@@ -1,10 +1,17 @@
 package com.johanlund.settings;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceFragment;
+import android.support.v7.preference.PreferenceManager;
+import android.support.v7.preference.SeekBarPreference;
+import android.util.Log;
+import android.view.Menu;
 import android.view.MenuItem;
 
 import com.johanlund.ibsfoodanalyzer.R;
+
+import java.util.Map;
 
 /**
  * Created by Johan on 2017-10-05.
@@ -28,11 +35,27 @@ public class GeneralSettingsActivity extends AppCompatPreferenceActivity {
         }
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.to_default_menu, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
     public boolean onOptionsItemSelected(MenuItem item) {
         int id = item.getItemId();
 
         if (id == android.R.id.home) {
             finish();
+        }
+        if (id == R.id.menu_to_default) {
+            SharedPreferences settings= PreferenceManager.getDefaultSharedPreferences(this);
+            settings.edit().remove("hours_break").commit();
+            //PreferenceManager.setDefaultValues(getApplicationContext(),R.xml.general_preferences,true);
+           // int d =  settings.getInt("hours_break", 10);
+            //SeekBarPreference yourSeekBar = (SeekBarPreference) findViewById(R.id.pref_hours_break);
+            //yourSeekBar.setCurrentValue(20);
+
+           // Log.d(getClass().getSimpleName(), Integer.toString(d));
         }
         return true;
     }
