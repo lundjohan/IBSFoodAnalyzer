f448f9dd64fd7f183aec257de221732bfe613a4b
Johan Lund <jolu1207@student.miun.se>
2018-05-02 12:24:35 +0200

    copied from updated tagswrapper

--- app/src/main/java/com/johanlund/statistics_avg/BmsWrapper.java
+++ app/src/main/java/com/johanlund/statistics_avg/BmsWrapper.java
@@ -1,11 +1,13 @@
 package com.johanlund.statistics_avg;
 
 import com.johanlund.base_classes.Tag;
+import com.johanlund.util.RatingTimes;
 import com.johanlund.util.ScoreTime;
 import com.johanlund.util.TagsWrapperBase;
 
 import org.threeten.bp.LocalDateTime;
 
+import java.sql.Wrapper;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,42 +39,44 @@ public class BmsWrapper implements TagsWrapperBase{
     }
 
     /**
-     * Except for chunkend this is basically a copy of TagsWrapper makeTagsWrappers.
+     * Except for chunkend this is basically a copy of TagsWrapper makeTagsWrappers, only the latter is tested.
      * TODO use smaller methods together
-     * @param tags1
+     * @param allTags
      * @param scoreTimes
      * @param breaks
      * @return
      */
-    public static List<TagsWrapperBase> makeBmsWrappers(List<Tag> tags1, List<ScoreTime> scoreTimes,
+    public static List<TagsWrapperBase> makeBmsWrappers(List<Tag> allTags, List<ScoreTime> scoreTimes,
                                                          List<LocalDateTime> breaks) {
         List<TagsWrapperBase>toReturn = new ArrayList<>();
         int indTags = 0;
         int indRatings = 0;
-        for (LocalDateTime b: breaks){
+        for (int i = 0; i < breaks.size(); i++){
+            LocalDateTime b = breaks.get(i);
             List<Tag>tags = new ArrayList<>();
-            List<ScoreTime>bms = new ArrayList<>();
-            for (int i = indTags;i<tags1.size();i++){
-                if (b.isBefore(tags1.get(i).getTime())){
+            List<ScoreTime>ratings = new ArrayList<>();
+            while (indTags < allTags.size()){
+                if (b.isBefore(allTags.get(indTags).getTime())){
                     break;
                 }
                 else{
-                    tags.add(tags1.get(i));
+                    tags.add(allTags.get(indTags));
+                    indTags++;
                 }
-                indTags = i;
             }
-            for (int j = indRatings;j<scoreTimes.size();j++){
-                if (b.isBefore(scoreTimes.get(j).getTime())){
+
+            while (indRatings < scoreTimes.size()){
+                if (b.isBefore(scoreTimes.get(indRatings).getTime())){
                     break;
                 }
                 else{
-                    bms.add(scoreTimes.get(j));
+                    ratings.add(scoreTimes.get(indRatings));
+                    indRatings++;
                 }
-                indRatings = j;
             }
             //meaningless to make new RAtingTimes with only chunkEnd (this can happen when breaks are next to each other)
-            if (bms.size() > 0 || tags.size() > 0) {
-                toReturn.add(new BmsWrapper(tags, bms));
+            if (ratings.size() > 0 || tags.size() > 0) {
+                toReturn.add(new BmsWrapper(tags, ratings));
             }
         }
         return toReturn;
